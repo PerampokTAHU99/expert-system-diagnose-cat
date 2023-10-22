86963b4e2dae309fe7f3ea7372b23af50e1f1c7a
maulaniad <maulani.ad@gmail.com>
2023-10-22 14:45:41 +0700

    Menambahkan endpoint login dan diagnoses

--- object/api.php
+++ object/api.php
@@ -1,12 +1,17 @@
 <?php
+require_once("auth.php");
+
 session_start();
+
+$request = $_SERVER['REQUEST_METHOD'];
 
 if (empty($_GET['method'])) {
     header('Content-Type: application/json', true, 404);
     echo json_encode(
         array(
             'Status' => 404,
-            'Reason' => 'The HTTP 404 Not Found response status code indicates that the server cannot find the requested resource. Links that lead to a 404 page are often called broken or dead links and can be subject to link rot.'
+            'Reason' => 'Alamat yang anda kunjungi tidak valid.
+            Untuk akses API harap masukan \'method\' sebagai parameter query.'
         )
     );
     die(1);
@@ -15,20 +20,49 @@ if (empty($_GET['method'])) {
 define('METHOD', $_GET['method']);
 
 switch (METHOD) {
+    case 'login':
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
+
+        header('Location: login.php');
+        break;
     case 'diseases':
         $_SESSION['idDisease'] = $_GET['idDisease'];
+
         header('Location: diseases.php');
         break;
     case 'symptoms':
         $_SESSION['idSymptom'] = $_GET['idSymptom'];
+
         header('Location: symptoms.php');
         break;
     case 'diagnoses':
+        auth();
 
+        if (empty($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] == false) {
+            header('Content-Type: application/json', true, 401);
+            echo json_encode(
+                array(
+                    'Status' => 401,
+                    'Reason' => 'Anda harus Login terlebih dahulu.'
+                )
+            );
+        } else {
+            $_SESSION['idDiagnose'] = $_GET['idDiagnose'];
+            header('Location: diagnoses.php');
+        }
         break;
     default:
-        # code...
-        break;
+        header('Content-Type: application/json', true, 404);
+        echo json_encode(
+            array(
+                'Status' => 404,
+                'Reason' => 'Endpoint API tidak ditemukan. Harap periksa method.'
+            )
+        );
+    break;
 }
+
+die(0);
 
 ?>
